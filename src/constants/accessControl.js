49a1477a2c861bdf91df6f57ecb113ac170bba49
Copernic0s <andresedmpq@gmail.com>
2026-04-29 12:17:32 -0500

    Reconnect support tracker and restore agent access

--- src/constants/accessControl.js
+++ src/constants/accessControl.js
@@ -105,7 +105,7 @@ export const resolveAccessProfile = (user) => {
     canEditInvoiceDetails: isManager,
     canCommentOnly: !isManager,
     canViewAllData: isManager,
-    canViewSupportTracker: isManager,
+    canViewSupportTracker: true,
     canViewInvoiceEntry: isManager,
     canViewActivityLogs: canViewActivityLogsFallback(email),
     hasScopedPortfolio: isManager ? true : agentScope.length > 0
