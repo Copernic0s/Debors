b046166c3ee34ca41ff791aa4f0df7e32ef34d5f
Copernic0s <andresedmpq@gmail.com>
2026-05-10 00:35:04 -0500

    Disable debugger attach in CMP runner

--- automation/run_cmp_bot.ps1
+++ automation/run_cmp_bot.ps1
@@ -9,7 +9,7 @@ $env:CMP_USER_DATA_DIR = "C:\Users\AndresMendez\AppData\Local\Google\Chrome\User
 $env:CMP_PROFILE_DIR = "Profile 8"
 $env:CMP_CLONE_PROFILE = "false"
 $env:CMP_REQUIRE_EXACT_PROFILE = "true"
-$env:CMP_DEBUGGER_ADDRESS = "127.0.0.1:9222"
+$env:CMP_DEBUGGER_ADDRESS = ""
 $env:CMP_HEADLESS = "false"
 $env:CMP_SEARCH_SETTLE_SECONDS = "3.5"
 $env:CMP_SEARCH_MAX_WAIT_SECONDS = "16"
@@ -28,8 +28,8 @@ $chromeArgs = @(
   "--profile-directory=""Profile 8""",
   "https://cmp-front.production.united-fuel.com/company"
 )
-Write-Host "Launching Chrome Profile 8 in debug mode..."
-Start-Process -FilePath $chromePath -ArgumentList $chromeArgs | Out-Null
-Start-Sleep -Seconds 3
+Write-Host "Launching Chrome Profile 8..."
+Start-Process -FilePath $chromePath -ArgumentList "--user-data-dir=`"C:\Users\AndresMendez\AppData\Local\Google\Chrome\User Data`"","--profile-directory=`"Profile 8`"","https://cmp-front.production.united-fuel.com/company" | Out-Null
+Start-Sleep -Seconds 4
 
 python .\automation\cmp_invoice_extractor.py
