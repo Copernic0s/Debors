cf80d72da9024683b8d326c9f3de39977b69b3f9
Copernic0s <andresedmpq@gmail.com>
2026-05-09 23:57:10 -0500

    Force live CMP profile in runner

--- automation/run_cmp_bot.ps1
+++ automation/run_cmp_bot.ps1
@@ -7,7 +7,7 @@ $env:CMP_INPUT_MODE = "zoho_sheet"
 $env:CMP_ZOHO_SHEET_NAME = "CS by Agent"
 $env:CMP_USER_DATA_DIR = "C:\Users\AndresMendez\AppData\Local\Google\Chrome\User Data"
 $env:CMP_PROFILE_DIR = "Profile 8"
-$env:CMP_CLONE_PROFILE = "true"
+$env:CMP_CLONE_PROFILE = "false"
 $env:CMP_REQUIRE_EXACT_PROFILE = "true"
 $env:CMP_HEADLESS = "false"
 $env:CMP_SEARCH_SETTLE_SECONDS = "3.5"
@@ -16,4 +16,7 @@ $env:CMP_SEARCH_KEYSTROKE_DELAY = "0.08"
 $env:CMP_DEBUG_NOT_FOUND = "true"
 
 Write-Host "Running CMP extractor with Profile 8..."
+Write-Host "Closing Chrome processes to avoid profile lock..."
+taskkill /IM chrome.exe /F | Out-Null
+Start-Sleep -Seconds 1
 python .\automation\cmp_invoice_extractor.py
